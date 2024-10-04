48ac5c3c9a6eb75c37ea2f6c8f84b808982d96bb
ahmidev <ahmid.aitouali@gmail.com>
2024-10-04 21:37:54 +0200

    test

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ const limiter = rateLimit({
 app.use(limiter);
 
 // Connexion à MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connexion à MongoDB réussie.'))
   .catch((err) => console.error('Connexion à MongoDB échouée.', err));
 
